fd33a98f070c9186ad9151e1f709a71fc937a676
Richard Käll <richard.kall@me.com>
2015-08-09 19:01:41 +0200

    Add keyboard shortcuts

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,1 +1,23 @@
-// Load this file for better performance
+chrome.commands.onCommand.addListener(function (command) {
+  'use strict';
+
+  switch (command) {
+    case 'save':
+      Pinboard.save();
+      break;
+    case 'read_later':
+      Pinboard.readLater();
+      break;
+    case 'all':
+      Pinboard.all();
+      break;
+    case 'unread':
+      Pinboard.unread();
+      break;
+    case 'random':
+      Pinboard.random();
+      break;
+    default:
+      break;
+  }
+});
